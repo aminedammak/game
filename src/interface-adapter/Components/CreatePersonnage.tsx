65f1a5cc5182020b7bc8a488e5ea9b5c9ddd26c9
aminedammak <aminedammak86@gmail.com>
2024-09-09 22:58:26 +0100

    Style modal

--- src/interface-adapter/Components/CreatePersonnage.tsx
+++ src/interface-adapter/Components/CreatePersonnage.tsx
@@ -15,17 +15,63 @@ function CreatePersonnage() {
     <div>
       <button onClick={openModal}>Create Personnage</button>
       {isModalOpen && (
-        <div className="modal">
-          <div className="modal-content">
+        <div
+          className="modal"
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            zIndex: 1000,
+          }}
+        >
+          <div
+            className="modal-content"
+            style={{
+              backgroundColor: "white",
+              padding: "20px",
+              borderRadius: "5px",
+              maxWidth: "500px",
+              width: "90%",
+              maxHeight: "90%",
+              overflowY: "auto",
+            }}
+          >
             <h2>Create Personnage</h2>
             <form>
-              <div>
-                <label htmlFor="name">Name:</label>
-                <input type="text" id="name" name="name" required />
+              <div style={{ marginBottom: "10px" }}>
+                <label
+                  htmlFor="name"
+                  style={{ display: "block", marginBottom: "5px" }}
+                >
+                  Name:
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  required
+                  style={{ width: "100%", padding: "5px" }}
+                />
               </div>
-              <div>
-                <label htmlFor="classe">Class:</label>
-                <select id="classe" name="classe" required>
+              <div style={{ marginBottom: "10px" }}>
+                <label
+                  htmlFor="classe"
+                  style={{ display: "block", marginBottom: "5px" }}
+                >
+                  Class:
+                </label>
+                <select
+                  id="classe"
+                  name="classe"
+                  required
+                  style={{ width: "100%", padding: "5px" }}
+                >
                   <option value="">Select a class</option>
                   <option value="Guerrier">Guerrier</option>
                   <option value="Chasseur">Chasseur</option>
@@ -33,10 +79,15 @@ function CreatePersonnage() {
                   <option value="Prêtre">Prêtre</option>
                 </select>
               </div>
-              <div>
+              <div style={{ marginBottom: "10px" }}>
                 <h3>Mastery Points:</h3>
-                <div>
-                  <label htmlFor="agility">Agility:</label>
+                <div style={{ marginBottom: "5px" }}>
+                  <label
+                    htmlFor="agility"
+                    style={{ display: "inline-block", width: "100px" }}
+                  >
+                    Agility:
+                  </label>
                   <input
                     type="number"
                     id="agility"
@@ -44,10 +95,16 @@ function CreatePersonnage() {
                     min="0"
                     max={5}
                     required
+                    style={{ width: "50px" }}
                   />
                 </div>
-                <div>
-                  <label htmlFor="strength">Strength:</label>
+                <div style={{ marginBottom: "5px" }}>
+                  <label
+                    htmlFor="strength"
+                    style={{ display: "inline-block", width: "100px" }}
+                  >
+                    Strength:
+                  </label>
                   <input
                     type="number"
                     id="strength"
@@ -55,10 +112,16 @@ function CreatePersonnage() {
                     min="0"
                     max={5}
                     required
+                    style={{ width: "50px" }}
                   />
                 </div>
-                <div>
-                  <label htmlFor="intelligence">Intelligence:</label>
+                <div style={{ marginBottom: "5px" }}>
+                  <label
+                    htmlFor="intelligence"
+                    style={{ display: "inline-block", width: "100px" }}
+                  >
+                    Intelligence:
+                  </label>
                   <input
                     type="number"
                     id="intelligence"
@@ -66,12 +129,20 @@ function CreatePersonnage() {
                     min="0"
                     max={5}
                     required
+                    style={{ width: "50px" }}
                   />
                 </div>
               </div>
-              <button type="submit">Create</button>
+              <button
+                type="submit"
+                style={{ marginRight: "10px", padding: "5px 10px" }}
+              >
+                Create
+              </button>
+              <button onClick={closeModal} style={{ padding: "5px 10px" }}>
+                Close
+              </button>
             </form>
-            <button onClick={closeModal}>Close</button>
           </div>
         </div>
       )}
