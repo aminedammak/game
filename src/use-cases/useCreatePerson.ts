7bbad932fbd863f1dd429094aa166a0afc0f2c4c
aminedammak <aminedammak86@gmail.com>
2024-09-11 05:45:27 +0100

    Block creation for duplication and max 10

--- src/use-cases/useCreatePerson.ts
+++ src/use-cases/useCreatePerson.ts
@@ -22,17 +22,26 @@ export const useCreatePerson = () => {
     const personnages = getAllPersonnages();
 
     // check if the name does not already exists in the list of personnages, otherwise show a message
-    // return {
-    //   status: "failure",
-    //   message:
-    //     "Vous ne pouvez pas utiliser plusieurs fois le même nom de personnage",
-    // };
+
+    const personnageExists = personnages.some(
+      (personnage) => personnage.name === name
+    );
+
+    if (personnageExists) {
+      return {
+        status: "failure",
+        message:
+          "Vous ne pouvez pas utiliser plusieurs fois le même nom de personnage",
+      };
+    }
 
     // check that the list is less than 10, otherwise show a message
-    // return {
-    //   status: "failure",
-    //   message: "Vous avez atteint la limite de 10 personnages par joueur",
-    // };
+    if (personnages.length >= 10) {
+      return {
+        status: "failure",
+        message: "Vous avez atteint la limite de 10 personnages par joueur",
+      };
+    }
 
     // if the above checks passes, proceed for the creation of personnage
     const newPersonnage = createPersonnage(name, classe, masteryPoints);
